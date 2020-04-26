3ff1b2a51c7e81bb67359571c892d28fc8716b41
Bogdan <bogdan.c.botez@student.unitbv.ro>
2020-04-26 16:25:02 +0300

    Added Adapter for BmpWriter.

--- IPDP/Resources/Writer/BmpWriter.cs
+++ IPDP/Resources/Writer/BmpWriter.cs
@@ -7,23 +7,33 @@ using System.Threading.Tasks;
 
 namespace IPDP.Resources.Writer
 {
+
     public class BmpWriter : IImageWriter
     {
+        protected static class BmpWriterAdapter
+        {
+
+            public static void WriteImage(Image image, string filename)
+            {
+                using (var bitmap = new Bitmap(image.Width, image.Height))
+                {
+
+                    for (var iRow = 0; iRow < image.Height; iRow += 1)
+                    {
+                        for (var iCol = 0; iCol < image.Width; iCol += 1)
+                        {
+                            var pixel = image[iRow, iCol];
+                            bitmap.SetPixel(iCol, iRow, Color.FromArgb(pixel.A, pixel.R, pixel.G, pixel.B));
+                        }
+                    }
+                    bitmap.Save(filename);
+                }
+            }
+
+        }
         public void WriteImage(Image image, string filename)
         {
-            using (var bitmap = new Bitmap(image.Width, image.Height))
-            {
-
-                for (var iRow = 0; iRow < image.Height; iRow += 1)
-                {
-                    for (var iCol = 0; iCol < image.Width; iCol += 1)
-                    {
-                        var pixel = image[iRow, iCol];
-                        bitmap.SetPixel(iCol, iRow, Color.FromArgb(pixel.A, pixel.R, pixel.G, pixel.B));
-                    }
-                }
-                bitmap.Save(filename);
-            }
+            BmpWriterAdapter.WriteImage(image, filename);
         }
     }
 }
